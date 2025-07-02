4537fab44cb00792e396f6071a8202d83c9cc713
oTheAnalyst <michaelvcumbo21@proton.me>
2025-07-02 14:48:58 -0400

    added in sql builder functionn

--- R/sql_functions.R
+++ R/sql_functions.R
@@ -4,7 +4,6 @@
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
-#
 #   https://r-pkgs.org
 #
 # Some useful keyboard shortcuts for package authoring:
@@ -12,53 +11,33 @@
 #   Install Package:           'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
-
-hello <- function() {
- # ggplot
- # summary tables
- # transformation
- # data imports
-  print("Analysis of something interesting")
+#
+write_to_sql <- function(name, data) {
+  driver <- RSQLite::SQLite()
+  file_name <- here::here("inst", "extdata","baltimoredata.sqlite3")
+  con <- DBI::dbConnect(driver, file_name)
+  RSQLite::dbWriteTable(con, name, data, overwrite = TRUE)
+  RSQLite::dbDisconnect(con)
 }
-
-
-
-
-write_to_sql <- function(data, name) {
-  driver <- RSQLite::dbDriver("SQLite")
-  sql_location <- here::here("inst", "extdata","district_of_columbia")
-  conn <- RSQLite::dbConnect(driver, sql_location)
-  RSQLite::dbWriteTable(conn, name, data, append = TRUE)
-  RSQLite::dbDisconnect(conn)
-}
-
 
 read_sql <- function(name) {
   driver <- RSQLite::dbDriver("SQLite")
-  sql_location <- here::here("inst", "extdata","district_of_columbia")
-  conn <- RSQLite::dbConnect(driver, sql_location)
-  table <- RSQLite::dbReadTable(conn,{{name}})
+  conn <- DBI::dbConnect(driver, sql_location)
+  table <- DBI::dbReadTable(conn,{{name}})
   return(dplyr::as_tibble(table))
   RSQLite::dbDisconnect(conn)
 }
 
 sql_list <- function() {
   driver <- RSQLite::dbDriver("SQLite")
-  sql_location <- here::here("inst", "extdata","district_of_columbia.sqlite3")
-  conn <- RSQLite::dbConnect(driver, sql_location)
-  DBI::dbListTables(conn)
+  conn <- RSQLite::dbConnect(driver, l_location)
+  table <- DBI::dbListTables(conn)
   return(table)
   RSQLite::dbDisconnect(conn)
 }
 
-  sql_location <- here::here("inst", "extdata","district_of_columbia.sqlite3")
-  conn <- RSQLite::dbConnect(driver, sql_location)
-  DBI::dbListTables(conn)
-sql_list()
-
 execute_sql <- function(statement) {
   driver <- RSQLite::dbDriver("SQLite")
-  sql_location <- here::here("inst", "extdata","district_of_columbia")
   conn <- RSQLite::dbConnect(driver, sql_location)
   RSQLite::dbExecute(conn, {{statement}})
   RSQLite::dbDisconnect(conn)
